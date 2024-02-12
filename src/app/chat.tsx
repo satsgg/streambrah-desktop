315dec0fcbcffefaf3375fad366e64566cb8f511
Chad Welch <cwelc001@gmail.com>
2024-02-12 12:15:42 -0500

    navbar title per route path

--- src/app/chat.tsx
+++ src/app/chat.tsx
@@ -1,6 +1,6 @@
 export default function Chat() {
   return (
-    <div className="flex flex-col w-1/4">
+    <div className="flex flex-col w-1/2">
       <div className="flex justify-center">
         <p>Chat</p>
       </div>
--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,9 +1,33 @@
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+
+const getTitle = (pathname: string) => {
+  if (pathname === "/") {
+    return "dashboard";
+  } else if (pathname === "/settings") {
+    return "settings";
+  } else if (pathname === "/apps") {
+    return "apps";
+  } else if (pathname === "/integrations/owncast") {
+    return "owncast";
+  } else {
+    return "";
+  }
+};
+
 export default function Navbar() {
+  const pathname = usePathname();
+  const [title, setTitle] = useState("dashboard");
+
+  useEffect(() => {
+    setTitle(getTitle(pathname));
+  }, [pathname]);
+
   return (
     <nav className="">
       <div className="flex px-4 py-2 justify-between">
         <div>
-          <p>Dashboard</p>
+          <p className="text-lg capitalize">{title}</p>
         </div>
         <div>
           <p>Right</p>
