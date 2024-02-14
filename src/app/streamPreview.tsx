f1b201d81e6320cd1e0fd5a661a4afb56497cab4
Chad Welch <cwelc001@gmail.com>
2024-02-14 08:16:38 -0500

    add stream info placeholders

--- src/app/streamPreview.tsx
+++ src/app/streamPreview.tsx
@@ -12,6 +12,21 @@ export default function StreamPreview() {
           />
         </video>
       </div>
+
+      <div className="flex justify-between p-2">
+        <div className="flex gap-2">
+          <div className="h-10 w-10 rounded-[50%] bg-red-500" />
+          <div className="flex flex-col">
+            <p className="text-semibold">Stream title</p>
+            <p className="text-sm">Stream description</p>
+            <p className="text-sm">Tags</p>
+          </div>
+        </div>
+
+        <div>
+          <p>Offline</p>
+        </div>
+      </div>
     </div>
   );
 }
